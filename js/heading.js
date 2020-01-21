1668a31b251c5969add49b75b8c2e43de2df992d
MrGregor228 <kadaner.grisha@gmail.com>
2020-01-21 12:32:51 +0200

    kugclc

---
+++ js/heading.js
@@ -0,0 +1,15 @@
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".navbar");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
+
+function parallax() {
+    var scrolled = $(window).scrollTop();
+    $('.bg').css('top', +(scrolled * 0.4) + 'px');
+}
+
+$(window).scroll(function (e) {
+    parallax();
+});
