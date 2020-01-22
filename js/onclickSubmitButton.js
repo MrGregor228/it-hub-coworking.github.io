e445260c1305e593496072efebf8a12922fc40a2
MrGregor228 <kadaner.grisha@gmail.com>
2020-01-22 17:07:38 +0200

    erbgojerg

---
+++ js/onclickSubmitButton.js
@@ -0,0 +1,4 @@
+function hideshow() {
+    document.getElementById("mG61Hd").style.cssText = "animation:hierrr 1s 1 linear;display:none;";
+    document.getElementById("shower").style.cssText = "display:block;animation:showwww 1s 1 linear;";
+}
