04c11476603693a7b28ad1184b5e406396e7ce36
MrGregor228 <kadaner.grisha@gmail.com>
2020-02-18 15:42:18 +0200

    рыкеок6л

--- js/langShowerMenu.js
+++ js/langShowerMenu.js
@@ -1,16 +1,10 @@
-let buttonIsOnThePage = setInterval(function () {
-    var buttonLangMenuShower = document.getElementById("landShow"),
-        menuShower = document.getElementById("langShowerMenu");
+var buttonLangMenuShower = document.getElementById("landShow"),
+    menuShower = document.getElementById("langShowerMenu");
 
 
-    buttonLangMenuShower.onclick = $(function () {
-        $("#landShow").click(function () {
-            $(menuShower).toggleClass("hiddddden");
-        });
+buttonLangMenuShower.onclick = $(function () {
+    $("#landShow").click(function () {
+        $(menuShower).toggleClass("hiddddden");
     });
-    console.log("Button is here!");
-}, 1000);
-
-setTimeout(function () {
-    clearInterval(buttonIsOnThePage);
-}, 1500);
+});
+console.log("Button is here!");
