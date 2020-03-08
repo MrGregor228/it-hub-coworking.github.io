f72dc82ff28cc7a2cde42e79af0cd6648c2f3a6a
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2020-03-08 21:39:28 +0200

    uctfkujcgv.kvh

---
+++ js/liveInternetCounter.js
@@ -0,0 +1,2 @@
+document.write('<a href="https://www.liveinternet.ru/click" ' + 'target="_blank"><img src="https://counter.yadro.ru/hit?t14.6;r' + escape(document.referrer) + ((typeof (screen) == 'undefined') ? '' :';s' + screen.width + '*' + screen.height + '*' + (screen.colorDepth ?
+screen.colorDepth : screen.pixelDepth)) + ';u' + escape(document.URL) + ';h' + escape(document.title.substring(0, 150)) + ';' + Math.random() + '" alt="" title="LiveInternet: показано число просмотров за 24' + ' часа, посетителей за 24 часа и за сегодня" ' + 'border="0" width="88" height="31"><\/a>');
